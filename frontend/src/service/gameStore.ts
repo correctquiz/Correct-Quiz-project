333b7b89c3374774ca9232f7807ff30b9c5c6691
thun <thunthim2546@gmail.com>
2025-11-27 21:50:37 +0700

    update

--- frontend/src/service/gameStore.ts
+++ frontend/src/service/gameStore.ts
@@ -1,5 +1,12 @@
-import { Writable, writable } from "svelte/store";
+import { get, Writable, writable } from "svelte/store";
 import { PlayerGame } from "./player/player";
-import { push } from "svelte-spa-router";
 
 export const playerGameStore: Writable<PlayerGame | null> = writable(null);
+
+  export function initializePlayerGame(
+    navigateFunction: (path: string) => void,
+  ) {
+    if (get(playerGameStore) === null) {
+      playerGameStore.set(new PlayerGame(navigateFunction));
+    }
+  }
